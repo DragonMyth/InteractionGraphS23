24d3ae86d6b9b97558a3586a0e1d6a9d5ccb1d50
DragonMyth <yunbo@yunbo-Ubuntu-18.04>
2023-08-25 15:48:57 -0400

    npc

--- motion_plausibility/featurizer.py
+++
@@ -1,9 +0,0 @@
-from fairmotion.tasks.motion_plausibility import featurizer
-
-
-class ZFeaturizer(featurizer.Featurizer):
-    def __init__(self, mean=None, std=None):
-        super().__init__(mean, std)
-
-    def featurize(self, z_prev, z):
-        return self.normalize(z_prev), self.normalize(z)
--- playground/load_interation.py
+++
@@ -1,5 +0,0 @@
-import pickle as pkl
-
-filename = "/home/yunbo/ScaDive/data/render_data/box_throw_ig_original/render_data.pkl"
-interaction = pkl.load(open(filename,'rb'))
-print(interaction)
